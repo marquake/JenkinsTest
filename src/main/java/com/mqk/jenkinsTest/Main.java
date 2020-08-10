f3a4b2b4b566c52b51b7b4316a73c5365de6a76f
marquake <marcosms@gmail.com>
2020-08-10 11:51:07 +0200

    añadidas clases de test

---
+++ src/main/java/com/mqk/jenkinsTest/Main.java
@@ -0,0 +1,21 @@
+package main.java.com.mqk.jenkinsTest;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("HOLA MUNDO JENKINS");
+	}
+
+	public int suma(int a, int b) {
+		return a + b;
+	}
+
+	public int resta(int a, int b) {
+		return a - b;
+	}
+
+	public int multiplicacion(int a, int b) {
+		return a * b;
+	}
+
+}
---
+++ test/TestJenkins.java
@@ -0,0 +1,89 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import main.java.com.mqk.jenkinsTest.Main;
+
+/**
+ * 
+ */
+
+/**
+ * @author A154308
+ *
+ */
+public class TestJenkins {
+
+	Main main = new Main();
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+//	/**
+//	 * Test method for {@link main.java.com.mqk.jenkinsTest.Main#main(java.lang.String[])}.
+//	 */
+//	@Test
+//	public void testMain() {
+//		fail("Not yet implemented");
+//	}
+
+	/**
+	 * Test method for {@link main.java.com.mqk.jenkinsTest.Main#suma(int, int)}.
+	 */
+	@Test
+	public void testSuma() {
+		int resultado = main.suma(5, 6);
+		
+		assertTrue(resultado == 11);
+	}
+
+	/**
+	 * Test method for {@link main.java.com.mqk.jenkinsTest.Main#resta(int, int)}.
+	 */
+	@Test
+	public void testResta() {
+int resultado = main.resta(22, 10);
+		
+		assertTrue(resultado == 12);
+	}
+
+	/**
+	 * Test method for {@link main.java.com.mqk.jenkinsTest.Main#multiplicacion(int, int)}.
+	 */
+	@Test
+	public void testMultiplicacion() {
+int resultado = main.multiplicacion(4, 12);
+		
+		assertTrue(resultado == 48);
+	}
+
+}
